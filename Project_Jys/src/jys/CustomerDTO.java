f99ae2fc952fd93a50d5fa2609d00722e5e8d47a
Suk66 <hs@localhost>
2024-11-29 12:04:48 +0900

    CustomerDTO

---
+++ Project_Jys/src/jys/CustomerDTO.java
@@ -0,0 +1,51 @@
+package jys;
+
+import java.sql.Date;
+
+public class CustomerDTO {
+	
+	private int cus_id;
+	private String cus_phone;
+	private int cus_point;
+	private Date cus_date;
+	
+	public CustomerDTO() {}
+
+	public int getCus_id() {
+		return cus_id;
+	}
+
+	public void setCus_id(int cus_id) {
+		this.cus_id = cus_id;
+	}
+
+	public String getCus_phone() {
+		return cus_phone;
+	}
+
+	public void setCus_phone(String cus_phone) {
+		this.cus_phone = cus_phone;
+	}
+
+	public int getCus_point() {
+		return cus_point;
+	}
+
+	public void setCus_point(int cus_point) {
+		this.cus_point = cus_point;
+	}
+
+	public Date getCus_date() {
+		return cus_date;
+	}
+
+	public void setCus_date(Date cus_date) {
+		this.cus_date = cus_date;
+	}
+
+	
+	
+	
+	
+
+}
